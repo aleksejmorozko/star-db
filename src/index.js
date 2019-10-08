06d51680e77d5766490b5320cd5480dbbc8846b7
Aleksey <Aleksey@M>
2019-10-08 23:42:09 +0300

    lesson_055

--- src/index.js
+++ src/index.js
@@ -1,15 +1,6 @@
-const getResource = async(url) => {
-    const res = await fetch(url);
-    if (!res.ok){
-        throw new Error(`could not fetch ${url}, recived ${res.status}`)
-    }
-    const body = await res.json();
-    return body;
-}
-getResource('https://swapi.co/api/people/154545444qwerwqer5')
-    .then((body) => {
-        console.log(body);
-    })
-    .catch((err) => {
-        console.error('could not fetch', err);
-    })
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/app';
+
+ReactDOM.render(<App />,
+  document.getElementById('root'));
