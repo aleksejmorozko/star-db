387724c302cf30ba54419ef42c016e2869ddf4d1
Aleksey <Aleksey@M>
2019-10-28 20:53:51 +0300

    lesson_065

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -10,7 +10,8 @@ import './app.css';
 export default class App extends Component {
 
   state = {
-    showRandomPlanet: true
+    showRandomPlanet: true,
+    selectedPerson: null
   };
 
   toggleRandomPlanet = () => {
@@ -21,6 +22,11 @@ export default class App extends Component {
     });
   };
 
+  onPersonSelected = (id) => {
+    this.setState({
+      selectedPerson: id
+    })
+  }
   render() {
 
     const planet = this.state.showRandomPlanet ?
@@ -40,10 +46,10 @@ export default class App extends Component {
 
         <div className="row mb2">
           <div className="col-md-6">
-            <ItemList />
+            <ItemList onItemSelected={this.onPersonSelected} />
           </div>
           <div className="col-md-6">
-            <PersonDetails />
+            <PersonDetails  personId={this.state.selectedPerson} />
           </div>
         </div>
       </div>
